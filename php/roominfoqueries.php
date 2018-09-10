e0f858f45988c48e404c92a752ae108a0f5b5d5d
Jonathan Whittle <jonathan.m.whittle@gmail.com>
2018-09-10 11:25:10 -0400

    ajax query handle

--- php/roominfoqueries.php
+++ php/roominfoqueries.php
@@ -2,7 +2,29 @@
 include 'inc/authenticate.php';
 if (!$conn) {
     die('Could not connect: ' . mysqli_error($conn));
+} else echo "Database accessed.";
+
+
+///////////////////////////////////////////////////////////
+//-----------------NORTON ROOM QUERY------------------
+//////////////////////////////////////////////////////////
+if($_GET['building'] === "Norton"){
+    $building = "norton_rooms";
+} elseif ($_GET['building'] === "Carver"){
+    $building = "carver_rooms";
+} elseif ($_GET['building'] === "Rankin"){
+    $building = "rankin_rooms";
+} elseif ($_GET['building'] === "Library"){
+    $building = "library_rooms";
+} elseif ($_GET['building'] === "Cook"){
+    $building = "cook_rooms";
 }
+$roomNumberQuery = "SELECT room_number FROM ?"; 
+$stmt = $conn->prepare($roomQuery); 
+$stmt->bind_params('s', $building);
+$result = $stmt->fecthAll(); 
+echo $result; 
+
 
 $query = "SELECT description FROM device_description WHERE device = ? AND building = ? AND room_code = ?";
 
