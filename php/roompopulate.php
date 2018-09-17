d50401f0d3ccffb2927ff10fb3114c1f2a018254
Jonathan Whittle <jonathan.m.whittle@gmail.com>
2018-09-17 12:51:10 -0400

    update php ajax

--- php/roompopulate.php
+++ php/roompopulate.php
@@ -6,19 +6,19 @@ if (empty($_GET['building'])){
     $search = trim($_GET['building']);
     switch($search){
         case "Norton":
-            $result = ["", 11, 12, 13, 14, 15, 16, 17, 20,101, 102, 103, 104, 105, 195, 201, 202, 203, 204, 205, 206, 207, 208, 209, 232];
+            $result = [11, 12, 13, 14, 15, 16, 17, 20,101, 102, 103, 104, 105, 195, 201, 202, 203, 204, 205, 206, 207, 208, 209, 232];
             break; 
         case "Carver":
-            $result = ["", 108, 135];
+            $result = [108, 135];
             break; 
         case "Rankin":
-            $result = ["", 101, 201];
+            $result = [101, 201];
             break; 
         case "Library":
-            $result = ["", "Crismon", "Curriculum Lab", "Mullins Room"];
+            $result = ["Crismon", "Curriculum Lab", "Mullins Room"];
             break; 
         case "Cook": 
-            $result = ["", 8, 221, 224, 'CCRH', 'IRH', 'Heeren Hall'];
+            $result = [8, 221, 224, 'CCRH', 'IRH', 'Heeren Hall'];
             break; 
     }
     echo json_encode($result); 
